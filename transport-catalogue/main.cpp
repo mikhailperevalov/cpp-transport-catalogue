864e6093137f70263cb29fcf8cd279fba127be95
Perevalov Mikhail <95629545+mikhailperevalov@users.noreply.github.com>
2022-05-06 09:43:36 +0300

    sprint 9.0

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,22 @@
-// место для вашего кода
+#include <iostream>
+#include <fstream>
+
+#include "input_reader.h"
+#include "transport_catalogue.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    transport_catalogue::TransportCatalogue catalogue;
+
+    if (1 < argc) {
+        ifstream in(argv[1]);
+        ofstream out(argv[2]);
+        transport_catalogue::parsing::ReadData(catalogue, in);
+        transport_catalogue::parsing::ReadAllRequestsAndPrint(catalogue, in, out);
+    } else {
+        transport_catalogue::parsing::ReadData(catalogue, cin);
+        transport_catalogue::parsing::ReadAllRequestsAndPrint(catalogue, cin, cout);
+    }
+}

f18c5ee1c90e392b91388b0c1ffbf663fc191657
Perevalov Mikhail <95629545+mikhailperevalov@users.noreply.github.com>
2022-05-06 09:44:56 +0300

    sprint 9.0

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,10 @@
-// место для вашего кода
+#pragma once
+
+#include "transport_catalogue.h"
+
+namespace transport_catalogue {
+    namespace printer {
+        void PrintRouteInfo(const TransportCatalogue &catalogue, const std::string &route_name, std::ostream &out);
+        void PrintStopInfo(const TransportCatalogue &catalogue, const std::string &stop_name, std::ostream &out);
+    } 
+} 
